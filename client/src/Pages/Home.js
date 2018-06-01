cda544520f7773e18a856e3fae78f99a81ee69ed
Victoria League <vleague2@gmail.com>
2018-06-01 11:38:15 -0400

    initial file structure

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const Home = () => (
+    
+    <h1>
+        <span role="img" aria-label="Face With Rolling Eyes Emoji">
+        🙄
+        </span>
+    </h1>
+  );
+  
+export default Home;
