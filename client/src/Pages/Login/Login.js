7af9e6ba69703735c0f938038ac877242874fc18
Victoria League <vleague2@gmail.com>
2018-06-03 14:02:32 -0400

    started login page template

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./Login.css";
+import {Container, Row, Col} from "../../Components/Grid";
 
 const Login = () => (
-    <h1> Test </h1>
+    <div>
+        <Container>
+            <Row>
+                <Col size="6">
+                    <h1> Test </h1>
+                </Col>
+
+                <Col size="6">
+                    <h1> Test 2 </h1>
+                </Col>  
+            </Row>
+        </Container>
+    </div>
 )
 
 export default Login;
