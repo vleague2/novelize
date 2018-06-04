2c5c970e055eb853e3e4c78571bb932327928598
Victoria League <vleague2@gmail.com>
2018-06-04 19:53:12 -0400

    you can now indent

--- client/src/Pages/Editor/Editor.js
+++ client/src/Pages/Editor/Editor.js
@@ -136,17 +136,25 @@ class EditorPage extends Component {
                             id="textEditor"
                             
                             init={{
-                            plugins: [
-                                'advlist autolink lists link image charmap print preview anchor textcolor',
-                                'searchreplace visualblocks code fullscreen',
-                                'insertdatetime media table contextmenu paste code help wordcount'
-                            ],
-                            toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
-                            menubar: false,
-                            content_css: [
-                                '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-                                '//www.tinymce.com/css/codepen.min.css'],
-                            
+                                plugins: [
+                                    'advlist autolink lists link image charmap print preview anchor textcolor',
+                                    'searchreplace visualblocks code fullscreen',
+                                    'insertdatetime media table contextmenu paste code help wordcount'
+                                ],
+                                toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+                                menubar: false,
+                                content_css: [
+                                    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+                                    '//www.tinymce.com/css/codepen.min.css'],
+                                setup: function(ed) {
+                                    ed.on('keydown', function(event) {
+                                        if (event.keyCode == 9) { // tab pressed
+                                            ed.execCommand('mceInsertContent', false, '&emsp;'); 
+                                            event.preventDefault();
+                                            return false;
+                                        }
+                                    });
+                                }
                             }}
                             onChange={this.handleEditorChange}
                         />
