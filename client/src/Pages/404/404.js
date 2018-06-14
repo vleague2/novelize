af554ffa1a9d570cd01cd5d18cac9af4cafce00f
Victoria League <vleague2@gmail.com>
2018-06-13 20:54:21 -0400

    added a 404

---
+++ client/src/Pages/404/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ErrorPage = () => (
+    <div className="text-center mt-5">
+        <h1>Oops! That page doesn't exist!</h1>
+    </div>
+)
+
+export default ErrorPage;
